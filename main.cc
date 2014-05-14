f5a829165a280672cbb2822450420bf9729eff1e
l0stman <endian.sign@gmail.com>
2014-05-14 19:29:33 +0300

    Replace the some ranges with shortcuts.

--- main.cc
+++ main.cc
@@ -181,8 +181,8 @@ UpdateVillStrategy(const vector<double>& equity,
 int
 main(int argc, char *argv[])
 {
-        Range hero("AcJc,KcJc,QcJc,JcTc,5c4c,KcJh,QcJh,KdJc,QdJc,5d4d,KdJh,QdJh,KhJc,QhJc,Ah2h,Ah5h,Ah8h,AhJh,KhJh,QhJh,JhTh,AsTc,KsJc,QsJc,AsTd,AsTh,KsJh,QsJh,Qs6s,QsTs,Ts7s,9s7s,8s7s,3d3c,3h3c,3h3d,6s6c,6d6c,6d6s,AsAc,AdAc,AhAc");
-        Range vill("44,55,77,88,99,TT,JJ,A3s,A6s,K6s,KJs,Q6s,QJs,J2s,J3s,J4s,J5s,J6s,J7s,J8s,J9s,JTs,62s,63s,64s,A6o,K6o,Q6o,J4o,J5o,J6o,J7o,J8o,J9o,JTo,T6o,96o,86o,76o,65o,Ts4s,Ts5s,Ts6s,Ts7s,Ts8s,Ts9s,9s4s,9s5s,9s6s,9s7s,9s8s,8s4s,8s5s,8s6s,8s7s,7s6s,6s5s,3d3c,6d6c,QsQc,QdQc,QhQc,KsKc,KdKc,KhKc,AsAc,AdAc,AhAc");
+        Range hero("AcJc,KcJc,QcJc,JcTc,5c4c,KcJh,QcJh,KdJc,QdJc,5d4d,KdJh,QdJh,KhJc,QhJc,Ah2h,Ah5h,Ah8h,AhJh,KhJh,QhJh,JhTh,AsTc,KsJc,QsJc,AsTd,AsTh,KsJh,QsJh,Qs6s-QsTs,Ts7s,9s7s,8s7s,3d3c,3h3c,3h3d,6s6c,6d6c,6d6s,AsAc,AdAc,AhAc");
+        Range vill("44,55,77-JJ,A3s,A6s,K6s,KJs,Q6s,QJs,J2s+,62s-64s,A6o,K6o,Q6o,J4o+,T6o,96o,86o,76o,65o,Ts4s+,9s4s+,8s4s+,7s6s,6s5s,3d3c,6d6c,QsQc,QdQc,QhQc,KsKc,KdKc,KhKc,AsAc,AdAc,AhAc");
         CardSet board("Js6h3sJd2d");
         vector<CardSet> hands;
         size_t hsiz = AddRange(hero, board, hands);
@@ -207,12 +207,12 @@ main(int argc, char *argv[])
         fprintf(stderr, "Hero EV = %.4f\n", EV);
         fprintf(stderr, "Hero:\n");
         for (size_t h = 0; h < hsiz; h++)
-                if (bet_prob[h] > 0.05) {
+                if (bet_prob[h] >= 0.05) {
                 fprintf(stderr, "%s: %.4f, EV = %.4f\n", hands[h].str().c_str(), bet_prob[h], bet_prob[h]*EVHeroBets(h, equity, call_prob, stack, pot, stack)+(1-bet_prob[h])*stack);
         }
         fprintf(stderr, "Villain:\n");
         for (size_t v = 0; v < vsiz; v++)
-                if (call_prob[v] > 0.05) {
+                if (call_prob[v] >= 0.05) {
                 fprintf(stderr, "%s: %.4f, EV = %.4f\n", hands[hsiz+v].str().c_str(), call_prob[v], call_prob[v]*EVVillCalls(v, equity, bet_prob, stack, pot, stack)+(1-call_prob[v])*stack);
         }
         return 0;
