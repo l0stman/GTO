fbd138e4e93b34ce8e479e0cfd096e41e935e08c
l0stman <endian.sign@gmail.com>
2014-06-02 12:19:45 +0300

    Replace the node class by a struct so we can call directly the utility function at a leaf instead of doing a dynamic dispatch.

--- cfr.lisp
+++ cfr.lisp
@@ -4,108 +4,88 @@
   "Represents the players in the game: HERO, VILLAIN or NOBODY."
   '(member hero villain nobody))
 
-(utils:defconstant +empty-array+ (vector))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun make-double-array (dims &key (initial-element 0.0d0))
+    (make-array dims
+                :element-type 'double-float
+                :initial-element initial-element)))
 
-(defclass node ()
-  ((name
-    :type simple-string
-    :initarg :name
-    :reader name
-    :initform (error "Need to supply a name")
-    :documentation "Name of the node.")
-   (active-player
-    :type player
-    :initarg :active-player
-    :reader active-player
-    :initform (error "Need to supply an active player.")
-    :documentation "The player that should play at the given node.")
-   (children
-    :type (simple-array node)
-    :initarg :children
-    :accessor children
-    :initform +empty-array+
-    :documentation "Array containing the children of the node.")
-   (infoset
-    :type T
-    :initarg :infoset
-    :reader infoset
-    :documentation "Optional information set that can be added to the node.")
-   (regret-sum
-    :type (simple-array double-float)
-    :reader regret-sum
-    :documentation "Array containing the sum of regrets of each action.")
-   (strategy
-    :type (simple-array double-float)
-    :reader strategy
-    :documentation "Array of probabilities of taking each action.")
-   (strategy-sum
-    :type (simple-array double-float)
-    :reader strategy-sum
-    :documentation "Array of sum of all the previous strategies.")))
+(utils:defconstant +empty-children+ (vector))
+(utils:defconstant +empty-double-array+ (make-double-array 0))
 
-(defmethod initialize-instance :before ((n node) &key name
-                                                      active-player
-                                                      num-states
-                                                      children)
-  (unless (typep name 'simple-string)
-    (error "NAME should be a string, not ~S" name))
-  (unless (typep active-player 'player)
-    (error "ACTIVE-PLAYER should be a PLAYER, not ~S" active-player))
-  (unless (or (null children) (typep children '(simple-array node)))
-    (error "CHILDREN should an array of node, not ~S" children))
-  (unless (or (null num-states) (typep num-states '(integer 0)))
-    (error "NUM-STATES should be a non-negative integer, not ~S"
-           num-states)))
+(defstruct (node (:constructor %make-node) (:copier) (:conc-name))
+  "Represents a node in the game-tree.
+NAME            : Name of the node.
+ACTIVE-PLAYER   : The player that should play at the given node.
+CHILDREN        : Array containing the children of the node.
+REGRET-SUM      : Array containing the sum of regrets of each action.
+STRATEGY        : Array of probabilities of taking each action.
+STRATEGY-SUM	: Array containing the sum of all the previous strategies.
+UTILITY         : Utility function for a leaf node."
+  (name (error "Need to supply a name.")
+   :type simple-string
+   :read-only t)
+  (active-player (error "Need to supply an active player.")
+   :type player
+   :read-only t)
+  (children +empty-children+
+   :type (simple-array node)
+   :read-only t)
+  (utility NIL
+   :type (or null (function (player integer integer) double-float))
+   :read-only t)
+  (regret-sum +empty-double-array+
+   :type (simple-array double-float)
+   :read-only t)
+  (strategy +empty-double-array+
+   :type (simple-array double-float)
+   :read-only t)
+  (strategy-sum +empty-double-array+
+   :type (simple-array double-float)
+   :read-only t))
 
-(defmethod initialize-instance :after ((n node)
-                                       &key num-states
-                                       &aux (len (length (children n))))
-  (with-slots (regret-sum strategy-sum strategy) n
-    (if (plusp len)
-        (let ((dims (list num-states len)))
-          (setf regret-sum   (make-double-array dims)
-                strategy-sum (make-double-array dims)
-                strategy     (make-double-array
-                              dims :initial-element (/ 1.0d0 len))))
-        (let ((arr (make-double-array 0)))
-          (setf regret-sum   arr
-                strategy-sum arr
-                strategy     arr)))))
-
-(defun make-node (name &optional (active-player 'nobody)
-                                 (num-states 0)
-                                 (children +empty-array+)
-                                 (infoset NIL))
+(defun make-node (name active-player num-states children)
   "Create a new node with name NAME and an active player
-ACTIVE-PLAYER.  CHILDREN is an array of nodes that are the children of
-the current one. NUM-STATES is the number of states of the current
-active player.  Each state for the active player is referred to by an
-unique id between 0 and NUM-OPP-STATES - 1. INFOSET is an optional
-information set to be stored at the node."
-  (make-instance 'node
-                 :name name
-                 :active-player active-player
-                 :children children
-                 :num-states num-states
-                 :infoset infoset))
+ACTIVE-PLAYER.  CHILDREN is a non-empty array of nodes that are the
+children of the current one. NUM-STATES is the number of states of the
+current active player.  Each state for the active player is referred
+to by an unique id between 0 and NUM-STATES - 1."
+  (check-type name simple-string)
+  (check-type active-player (member hero villain))
+  (check-type num-states (integer 0))
+  (check-type children (simple-array node))
+  (when (zerop (length children))
+    (error "CHILDREN should be a non-empty array of nodes."))
+  (let ((dims (list num-states (length children))))
+    (%make-node :name name
+                :active-player active-player
+                :children children
+                :regret-sum (make-double-array dims)
+                :strategy-sum (make-double-array dims)
+                :strategy (make-double-array
+                           dims :initial-element (/ 1.0d0 (length children))))))
 
-(defun make-double-array (dims &key (initial-element 0.0d0))
-  (make-array dims
-              :element-type 'double-float
-              :initial-element initial-element))
+(defun make-leaf (name utility)
+  "Like `make-node', create a new leaf with name NAME. UTILITY is a
+  function that takes three arguments -- PLAYER, PID, OID -- and
+  returns the utility of PLAYER at this terminal node.  PID is the
+  state id of PLAYER and OID the state id of his opponent."
+  (check-type name simple-string)
+  (check-type utility function)
+  (%make-node :name name :active-player 'nobody :utility utility))
 
 (defun leafp (n)
   "Return true if N is a leaf node."
-  (and (typep n 'node) (eq (children n) +empty-array+)))
+  (and (typep n 'node) (eq (children n) +empty-children+)))
 
 (defun average-strategy (node)
   "Return the average info set mixed strategy across all iterations.
 The returned value is a two-dimensional array.  The first dimension
 corresponds to the state id of the active player and the second one to
 the action taken at the node."
-  (declare (node node))
-  (with-slots (strategy-sum) node
-    (unless (eq strategy-sum +empty-array+)
+  (check-type node node)
+  (let ((strategy-sum (strategy-sum node)))
+    (unless (eq strategy-sum +empty-children+)
       (let* ((dims (array-dimensions strategy-sum))
              (avg (make-double-array dims)))
         (destructuring-bind (nstates nactions) dims
@@ -119,12 +99,6 @@ the action taken at the node."
                           (/ 1.0d0 nactions)))))))
         avg))))
 
-(defgeneric utility (node player pid oid)
-  (:documentation
-   "Return the utility of PLAYER on a given leaf NODE. PID is the
-   state id of the active player and OID the state id of his
-   opponent."))
-
 (defun cfr (node player pprob oprob pid oid)
   "Return the utility of NODE for a given PLAYER using the
 counterfactual regret minimization algorithm. PPROB and OPROB are the
@@ -134,11 +108,14 @@ his opponent."
   (declare (optimize speed)
            (double-float pprob oprob)
            #+sbcl (sb-ext:muffle-conditions sb-ext:compiler-note))
-  (with-slots (children strategy regret-sum strategy-sum) node
+  (let ((children (children node))
+        (strategy (strategy node))
+        (regret-sum (regret-sum node))
+        (strategy-sum (strategy-sum node)))
     (declare ((simple-array node) children)
              ((simple-array double-float) strategy regret-sum strategy-sum))
-    (when (eq children +empty-array+)
-      (return-from cfr (utility node player pid oid)))
+    (when (eq children +empty-children+)
+      (return-from cfr (funcall (the function (utility node)) player pid oid)))
     (let ((utils (the (simple-array double-float)
                       (make-double-array (length children))))
           (id (if (eq (active-player node) player) pid oid))
